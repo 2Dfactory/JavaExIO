38347798236232bcbc93049523c443c18aec9566
gittesteur <TestGame@orange.fr>
2018-01-11 23:27:43 +0100

    Test sérialisation

---
+++ IO/src/Serialisation/MainFileIOS1.java
@@ -0,0 +1,58 @@
+package Serialisation;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class MainFileIOS1 {
+
+	public static void main(String[] args) {
+
+		ObjectInputStream ois = null;
+		ObjectOutputStream oos = null;
+		
+		try{
+			
+			oos = new ObjectOutputStream(new BufferedOutputStream(
+					new FileOutputStream(new File("Files/game.txt"))));
+			
+			oos.writeObject(new Game("Assassin Creed", "Aventure", 45.69));
+			oos.writeObject(new Game("Tomb Raider", "Plateforme", 23.45));
+			oos.writeObject(new Game("Tetris", "Strategie", 2.5));
+			
+			oos.close();
+			
+			ois = new ObjectInputStream(new BufferedInputStream(
+					new FileInputStream(new File("Files/game.txt"))));
+			
+			try{
+				System.out.println("Affichage des jeux");
+				System.out.println("*******************************");
+				System.out.println(((Game) ois.readObject()).toString());
+				System.out.println(((Game) ois.readObject()).toString());
+				System.out.println(((Game) ois.readObject()).toString());
+			}
+			catch(ClassNotFoundException c){
+				c.printStackTrace();
+			}
+			
+		}
+		catch(FileNotFoundException f){
+			f.printStackTrace();
+		}
+		catch(IOException io){
+			io.printStackTrace();
+		}
+		
+		
+		
+		
+	}
+
+}
