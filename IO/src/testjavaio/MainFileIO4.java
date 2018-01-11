3207c9f7e66fa055c8b1f345b3049fa9d9f284e6
vzlskl <vzlskl@DL30TYKZ1.Europe.DelphiAuto.net>
2018-01-11 17:02:53 +0100

    Essai IO avec DataStream

---
+++ IO/src/testjavaio/MainFileIO4.java
@@ -0,0 +1,61 @@
+package testjavaio;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
+public class MainFileIO4 {
+
+	public static void main(String[] args) {
+		
+		DataInputStream dis = null;
+		DataOutputStream dos = null;
+		
+		try{
+			
+			dos = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(new File("Files/test6.txt"))));
+			
+			dos.writeBoolean(true);
+			dos.writeByte(100);
+			dos.writeChar('c');
+			dos.writeDouble(12.5);
+			dos.writeFloat(100.52f);
+			dos.writeInt(1024);
+			dos.writeLong(123456789654321L);
+			dos.writeShort(2);
+			dos.close();
+			
+			
+			dis = new DataInputStream(new BufferedInputStream(new FileInputStream(new File("Files/test6.txt"))));
+			
+			System.out.println(dis.readBoolean());
+			System.out.println(dis.readByte());
+			System.out.println(dis.readChar());
+			System.out.println(dis.readDouble());
+			System.out.println(dis.readFloat());
+			System.out.println(dis.readInt());
+			System.out.println(dis.readLong());
+			System.out.println(dis.readShort());
+			dis.close();
+			
+		}
+		catch(FileNotFoundException f){
+			f.printStackTrace();
+		}
+		catch(IOException io){
+			io.printStackTrace();
+		}
+		
+		
+		
+
+	}
+
+}
