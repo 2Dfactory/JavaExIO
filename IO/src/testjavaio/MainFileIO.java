bc368a0332c94170a68aa04cc944044a9609f114
vzlskl <vzlskl@DL30TYKZ1.Europe.DelphiAuto.net>
2018-01-11 15:09:52 +0100

    Codage du contenu du bloc finally. Ajout des commentaires

--- IO/src/testjavaio/MainFileIO.java
+++ IO/src/testjavaio/MainFileIO.java
@@ -10,29 +10,40 @@ public class MainFileIO {
 
 	public static void main(String[] args) {
 
-		//Création des flux de données
+		//Création des objets gérant les flux de données
+		//fis ==> file input stream (flux de données en provenance d'un fichier)
+		//fos ==> file output stream (flux de données en direction d'un fichier
 		FileInputStream fis = null;
 		FileOutputStream fos = null;
 		
 		try{
 			
+			//Instanciation des flux
 			fis = new FileInputStream(new File("Files/test.txt"));
 			fos = new FileOutputStream(new File("Files/test2.txt"));
 			
+			//Taille du buffer lu à chaque boucle
 			byte[] buff = new byte[8];
 			
+			//Variable stockant le nombre d'octets lus à chaque boucle
+			// n = -1 en fin de lecture
 			int n = 0;
 			
+			
+			//On boucle tant qu'il y a des données
 			while((n=fis.read(buff))>=0){
 				
+				//Ecriture des données dans le fichier cible
 				fos.write(buff);
 				
+				//Ecriture dans la console des données lues selon 2 format byte et char
 				for(byte bit : buff){
 					System.out.print("\t"+bit+"("+ (char)bit+")");
 				}
 				
 				System.out.println("");
 				
+				//Réinitialisation du buffer après chaque lecture
 				buff = new byte[8];
 			}
 			
@@ -40,12 +51,31 @@ public class MainFileIO {
 			
 		}
 		catch(FileNotFoundException f){
+			
+			//Exception si fichier inexistant
 			f.printStackTrace();
 		}
 		catch(IOException io){
+			
+			//Exception sur erreur de lecture ou d'écriture
 			io.printStackTrace();
 		}
 		finally {
+			
+			//Fermeture des flux dans un bloc finally
+			try{
+				if(fis != null) fis.close();
+			}
+			catch(IOException io){
+				io.printStackTrace();
+			}
+			
+			try{
+				if(fos != null) fos.close();
+			}
+			catch(IOException io){
+				io.printStackTrace();
+			}
 			
 		}
 		
