03d20e8ebd7fca50264734949eee043427e95726
vzlskl <vzlskl@DL30TYKZ1.Europe.DelphiAuto.net>
2018-01-15 16:36:49 +0100

    Test avec CharArray(writer/reader) et String(Writer/Reader)

---
+++ IO/src/TestChWriterStrWriter/MainChrRdWr.java
@@ -0,0 +1,49 @@
+package TestChWriterStrWriter;
+
+import java.io.CharArrayReader;
+import java.io.CharArrayWriter;
+import java.io.IOException;
+
+
+//Essai CharArrayWriter & CharArrayReader
+public class MainChrRdWr {
+
+	public static void main(String[] args) {
+
+		CharArrayWriter caw = new CharArrayWriter();
+		CharArrayReader car = null;
+		
+		try{
+			
+			caw.write("Coucou les testeux!!");
+			//Appel à la méthode toString de notre objet de manière tacite
+			System.out.println(caw);
+			
+			//caw.close() n'a aucun effet sur le flux
+			//seul caw.reset() peut effacer les données
+			caw.close();
+			
+			//On passe un tableau de caractère à l'objet qui va lire le tampon
+			car = new CharArrayReader(caw.toCharArray());
+			
+			int i;
+			//On remet tous les caratères lus dans un String
+			String str = "";
+			
+			while((i=car.read())!= -1){
+				
+				str += (char) i;
+				
+				System.out.println(str);
+				
+			}
+			
+		}
+		catch(IOException io){
+			io.printStackTrace();
+		}
+		
+		
+	}
+
+}
---
+++ IO/src/TestChWriterStrWriter/MainStrRdWr.java
@@ -0,0 +1,47 @@
+package TestChWriterStrWriter;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+
+public class MainStrRdWr {
+
+	public static void main(String[] args) {
+		
+		StringWriter stw = new StringWriter();
+		StringReader str;
+		
+		try{
+			
+			stw.write("Coucou les testeux!!");
+			//Appel à la méthode toString de notre objet de manière tacite
+			System.out.println(stw);
+			
+			//stw.close() n'a aucun effet sur le flux
+			//seul stw.reset() peut effacer les données
+			stw.close();
+			
+			//On passe un tableau de caractère à l'objet qui va lire le tampon
+			str = new StringReader(stw.toString());
+			
+			int i;
+			//On remet tous les caratères lus dans un String
+			String st = "";
+			
+			while ((i = str.read()) != -1){
+				
+				st += (char) i;
+				
+				System.out.println(st);
+				
+			}
+			
+			
+		}
+		catch(IOException io){
+			io.printStackTrace();
+		}
+
+	}
+
+}
